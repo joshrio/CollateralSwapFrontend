73ce1ca622232efad151e096fdf8e792bfe8cf72
Josh Rio <riojosh@gmail.com>
2020-02-09 13:06:32 -0400

    quick format of code

--- src/controllers/Swap.js
+++ src/controllers/Swap.js
@@ -1,9 +1,9 @@
-import getAddresses from "../providers/getAddresses"
-import getGasPrice from "./GasStation"
+import getAddresses from "../providers/getAddresses";
+import getGasPrice from "./GasStation";
 
-import DSProxy from "../abi/DSProxy.json"
-import ProxyRegistry from "../abi/ProxyRegistry.json"
-import SwapActions from "../contracts/SwapActions.json"
+import DSProxy from "../abi/DSProxy.json";
+import ProxyRegistry from "../abi/ProxyRegistry.json";
+import SwapActions from "../contracts/SwapActions.json";
 
 /**
  * @param {  } web3 The web3 instance (usually metamask or their wallet)
@@ -12,54 +12,118 @@ import SwapActions from "../contracts/SwapActions.json"
  * @param { Boolean } ethToBat Whether the CDP is converting ETH collateral to BAT, or vice versa
  */
 export const performSwap = async (web3, account, cdpId, ethToBat) => {
-	let addresses = getAddresses()
+  let addresses = getAddresses();
 
-	let cdpManager = addresses.maker.cdpManager
-	let jug = addresses.maker.jug
-	let ethJoin = addresses.maker.ethJoin
-	let batJoin = addresses.maker.batJoin
-	let daiJoin = addresses.maker.daiJoin
+  let cdpManager = addresses.maker.cdpManager;
+  let jug = addresses.maker.jug;
+  let ethJoin = addresses.maker.ethJoin;
+  let batJoin = addresses.maker.batJoin;
+  let daiJoin = addresses.maker.daiJoin;
 
-	let daiAddress = addresses.tokens.dai
-	let batAddress = addresses.tokens.bat
-	let ethAddress = addresses.aave.ethAddress
+  let daiAddress = addresses.tokens.dai;
+  let batAddress = addresses.tokens.bat;
+  let ethAddress = addresses.aave.ethAddress;
 
-	let myProxyAddress = await getUsersProxy(web3, account)
-	
-	let data
-	if (ethToBat) {
-		data = web3.eth.abi.encodeParameters(
-			["address", "uint", "address", "address", "address", "address", "address", "address", "address", "bytes32"],
-			[myProxyAddress, cdpId, batAddress, ethAddress, cdpManager, ethJoin, daiJoin, batJoin, jug, web3.utils.utf8ToHex(addresses.maker.ilkBatA)]
-		)
-	} else {
-		data = web3.eth.abi.encodeParameters(
-			["address", "uint", "address", "address", "address", "address", "address", "address", "address", "bytes32"],
-			[myProxyAddress, cdpId, ethAddress, batAddress, cdpManager, ethJoin, daiJoin, batJoin, jug, web3.utils.utf8ToHex(addresses.maker.ilkEthA)]
-		)
-	}
+  let myProxyAddress = await getUsersProxy(web3, account);
 
-    let networkId = await web3.eth.net.getId()
-	let deployedNetwork = SwapActions.networks[networkId]
-    let swapActions = new web3.eth.Contract(SwapActions.abi, deployedNetwork && deployedNetwork.address)
-    
-	let flashloanCalldata = swapActions.methods.swapCollateral(cdpManager, addresses.collateralSwap.beta, daiAddress, cdpId, data).encodeABI()
+  let data;
+  if (ethToBat) {
+    data = web3.eth.abi.encodeParameters(
+      [
+        "address",
+        "uint",
+        "address",
+        "address",
+        "address",
+        "address",
+        "address",
+        "address",
+        "address",
+        "bytes32"
+      ],
+      [
+        myProxyAddress,
+        cdpId,
+        batAddress,
+        ethAddress,
+        cdpManager,
+        ethJoin,
+        daiJoin,
+        batJoin,
+        jug,
+        web3.utils.utf8ToHex(addresses.maker.ilkBatA)
+      ]
+    );
+  } else {
+    data = web3.eth.abi.encodeParameters(
+      [
+        "address",
+        "uint",
+        "address",
+        "address",
+        "address",
+        "address",
+        "address",
+        "address",
+        "address",
+        "bytes32"
+      ],
+      [
+        myProxyAddress,
+        cdpId,
+        ethAddress,
+        batAddress,
+        cdpManager,
+        ethJoin,
+        daiJoin,
+        batJoin,
+        jug,
+        web3.utils.utf8ToHex(addresses.maker.ilkEthA)
+      ]
+    );
+  }
 
-    let myProxyInstance = new web3.eth.Contract(DSProxy, myProxyAddress)
-    let gasPrice = await getGasPrice()
+  let networkId = await web3.eth.net.getId();
+  let deployedNetwork = SwapActions.networks[networkId];
+  let swapActions = new web3.eth.Contract(
+    SwapActions.abi,
+    deployedNetwork && deployedNetwork.address
+  );
 
-	let response = await myProxyInstance.methods.execute(addresses.collateralSwap.beta, flashloanCalldata).send({
-		from: account,
-		gas: 2000000,
-		gasPrice: web3.utils.toWei(`${gasPrice}`, "gwei")
-	})
+  let flashloanCalldata = swapActions.methods
+    .swapCollateral(
+      cdpManager,
+      addresses.collateralSwap.beta,
+      daiAddress,
+      cdpId,
+      data
+    )
+    .encodeABI();
 
-	console.log(response)
-	return response
-}
+  let myProxyInstance = new web3.eth.Contract(DSProxy, myProxyAddress);
+  let gasPrice = await getGasPrice();
+
+  let response = await myProxyInstance.methods
+    .execute(addresses.collateralSwap.beta, flashloanCalldata)
+    .send({
+      from: account,
+      gas: 2000000,
+      gasPrice: web3.utils.toWei(`${gasPrice}`, "gwei")
+    });
+
+  console.log(response);
+  return response;
+};
 
 export const getUsersProxy = async (web3, account) => {
-	let proxyRegistryInstance = new web3.eth.Contract(ProxyRegistry, getAddresses().maker.proxyRegistry)
-	let proxyAddress = await proxyRegistryInstance.methods.proxies(account).call()
-	return proxyAddress === "0x0000000000000000000000000000000000000000" ? null : proxyAddress
-}
+  let proxyRegistryInstance = new web3.eth.Contract(
+    ProxyRegistry,
+    getAddresses().maker.proxyRegistry
+  );
+  let proxyAddress = await proxyRegistryInstance.methods
+    .proxies(account)
+    .call();
+  return proxyAddress === "0x0000000000000000000000000000000000000000"
+    ? null
+    : proxyAddress;
+};
--- src/controllers/Web3.js
+++ src/controllers/Web3.js
@@ -1,12 +1,14 @@
-import getWeb3, { isEnabled } from "../providers/getWeb3"
+import getWeb3, { isEnabled } from "../providers/getWeb3";
 
 export const setupWeb3 = async () => {
-    try {
-        return await getWeb3()
-    } catch (error) {
-        console.error(`Failed to load web3, accounts, or contract: ${error.message}`)
-        return { web3: null, account: null }
-    }
-}
+  try {
+    return await getWeb3();
+  } catch (error) {
+    console.error(
+      `Failed to load web3, accounts, or contract: ${error.message}`
+    );
+    return { web3: null, account: null };
+  }
+};
 
-export const isWeb3Enabled = async () => await isEnabled()
+export const isWeb3Enabled = async () => await isEnabled();
